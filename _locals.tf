4af8f18c8b21d5c84e555a0d1b091d6f17414e86
Tarun Khare <tarunkh1690@gmail.com>
2023-03-03 01:35:32 +0530

    first commit

--- _locals.tf
+++ _locals.tf
@@ -1,7 +1,7 @@
 locals {
   common_tags = {
     project     = "terraform-workspaces-check"
-    environment = var.workspaces-name[var.aws_region]
+    environment = terraform.workspace
   }
   name_prefix = "${var.name}-${terraform.workspace}"
 }
